2c039cc8c0c88dd11a42dc9d891e789a745f8a0f
Mohith140306 <mohithdappadi@gmail.com>
2025-09-02 00:14:34 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -19,7 +19,7 @@ const Navbar = () => {
           {/* Logo */}
         <Link to="/" className="flex items-center space-x-2 hover-scale">
   <img
-    src="/sendlo.jpg"
+    src="/Screenshot 2025-09-02 001258.png"
     alt="Sendix Logo"
     className="w-6 h-6 md:w-8 md:h-8 rounded-lg object-contain"
   />
